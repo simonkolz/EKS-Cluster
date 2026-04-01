0839215d8b13d6b61acb1862a1cc7b80f9c54166
simonkolz <simonkolz11@gmail.com>
2026-04-01 19:55:16 +0100

    ecr.tf and pipeline change

--- terraform/modules/ecr.tf
+++ terraform/modules/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "url-app-hub" {
-  name                 = "url-app"
+  name                 = "url-app-hub"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
