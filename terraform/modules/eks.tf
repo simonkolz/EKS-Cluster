551608fb8b6c1aeec419ae121116156e41349fb9
simonkolz <simonkolz11@gmail.com>
2026-05-15 11:23:24 +0100

    pipeline fix

--- terraform/modules/eks.tf
+++ terraform/modules/eks.tf
@@ -39,3 +39,4 @@ module "eks" {
 
   tags = local.tags
 }
+
--- terraform/modules/helm.tf
+++ terraform/modules/helm.tf
@@ -85,7 +85,7 @@ resource "helm_release" "prometheus" {
   name             = "prometheus"
   repository       = "https://prometheus-community.github.io/helm-charts"
   chart            = "kube-prometheus-stack"
-  version          = "55.5.0"
+  version          = "69.3.0"
 
   namespace        = "monitoring"
   create_namespace = true
